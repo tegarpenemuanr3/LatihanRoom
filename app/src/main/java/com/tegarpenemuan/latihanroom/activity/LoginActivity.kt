062dd79660fde889d691f2f09a3bb77f4a7d7108
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-04-13 22:26:58 +0700

    Login

--- app/src/main/java/com/tegarpenemuan/latihanroom/activity/LoginActivity.kt
+++ app/src/main/java/com/tegarpenemuan/latihanroom/activity/LoginActivity.kt
@@ -3,20 +3,46 @@ package com.tegarpenemuan.latihanroom.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.tegarpenemuan.latihanroom.database.MyNoteDatabase
 import com.tegarpenemuan.latihanroom.databinding.ActivityLoginBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private var db: MyNoteDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = MyNoteDatabase.getInstance(applicationContext.applicationContext)
+
         binding.tvRegister.setOnClickListener {
-            startActivity(Intent(applicationContext,RegisterActivity::class.java))
+            startActivity(Intent(applicationContext, RegisterActivity::class.java))
         }
 
+        binding.btnLogin.setOnClickListener {
+            getUser(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+        }
+    }
+
+    fun getUser(email: String, password: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val user = db?.userDao()?.getUser(email = email, password = password)
+            runOnUiThread {
+                user?.let {
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                } ?: run {
+                    Toast.makeText(this@LoginActivity, "User tidak ditemukan", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
     }
 }
